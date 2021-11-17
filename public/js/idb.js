0cbfa398f7d0bf47030354626fa52e278ac04f9a
Tony Rivera <rtonycloud@gmail.com>
2021-11-16 19:08:32 -0500

    fixed service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -47,7 +47,7 @@ request.onupgradeneeded = function(event) {
     getAll.onsuccess = function() {
       // if there was data in indexedDb's store, let's send it to the api server
         if (getAll.result.length > 0) {
-        fetch('/api/budgets', {
+          fetch('/api/transaction/bulk', {
             method: 'POST',
             body: JSON.stringify(getAll.result),
             headers: {
